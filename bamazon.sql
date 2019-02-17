8c24e5539133c735a985af7bc69805c83af54152
mike929 <mike929@gmail.com>
2019-02-16 22:37:36 -0500

    addingLogic

---
+++ bamazon.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM bamazon.products
+LIMIT 0, 1000
+
+-- Date: 2019-02-14 18:32
+*/
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (1,'Canon 60D','Electronics',450,5);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (2,'Canon 50mm','Electronics',99.99,10);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (3,'Nikon 650D','Electronics',425.99,7);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (4,'Canon 70-200 2.8','Electronics',950.99,15);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (5,'Tamron SP 24-70mm','Electronics',799,8);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (6,'Canon EOS 6D Mark II','Electronics',1700,18);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (7,'Canon EOS 5D Mark IV','Electronics',2800,9);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (8,'Sony Alpha a7 Mirrorless Digital Camera','Electronics',1000.99,12);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (9,'Sony a7R III Mirrorless Camera','Electronics',3000,6);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (10,'Sony Planar T FE 50mm F1.4 ZA Lens','Electronics',1300,3);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (11,'Sony FE 70-200mm f/2.8 GM OSS Lens','Electronics',2500,7);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,88 +1,169 @@
 var inquirer = require('inquirer');
+var mysql = require('mysql');
 const cTable = require('console.table');
-const con = require('./connection');
+var Table = require('cli-table3');
+var figlet = require('figlet');
 
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "bamazon"
+  });
   
   con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
-   startBamazon();
+    console.log("We are Connected!");
   });
 
-// const questions = [{
-//         type: 'List',
-//         name: 'command',
-//         messages: 'List a set of menu options',
-//         choices: [
-//             'View products for Sale',
-//             'Quit'
-//         ]
-// }];
-
-// inquirer.prompt(questions)
-//     .then(function (data) {
-//         console.log('Data: ', data);
-//     });
 
 
-
-function startBamazon(){
-
-    con.query('SELECT * FROM products', function(err, results){
-        if(err){
-            console.log(err, "Something went wrong!")
+startBamazon();
+    
+    
+    // function to show list of available products to purchase
+    function startBamazon() {
+ 
+figlet('Welcome to BAMAZON', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+        console.log('\n\tHere is the current list of items available for purchase: ');
+        // create new table
+        var table = new Table({
+            head: ['Item ID', 'Product Name', 'Price', 'Quantity']
+        });
+    
+        // get product info from bamazon DB, products table
+        con.query("SELECT * FROM products", function (err, res) {
+            for (var i = 0; i < res.length; i++) {
+                table.push(
+                    [res[i].id, res[i].product_name, res[i].price, res[i].stock_quantity]
+                );
+            }
+            console.log(table.toString());
+            // call whatToBuy function
+            whatToBuy();
+        })
+    };
+    
+    // function to ask the user what they would like to purchase
+    function whatToBuy() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "itemID",
+                message: "Enter the Item ID of the item would you like to purchase",
+                validate: function (input) {
+                    if (isNaN(input)) {
+                        return "Enter the product number";
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        ]).then(function (user) {
+            itemToBuy = user.itemID;
+            // after user input is entered, call checkIfItem function
+            checkIfItem();
+        });
+    };
+    
+    // function to check if valid item #
+    function checkIfItem() {
+        // query bamazon DB for the entered item
+        con.query("SELECT id FROM products WHERE id=?", [itemToBuy], function (err, res) {
+            if (err) throw err;
+            // if itemToBuy exists in the databse, call howManyToBuy function
+            else if (res[0].id == itemToBuy) {
+                // call update Quantity function
+                howManyToBuy();
+            }
+            // if item does not exist in database, ask user to enter valid ID and go back to product list
+            else {
+                console.log("Please enter a valid item ID!");
+                console.log('\n*******************');
+                startBamazon();
+            }
+        });
+    }
+    
+    // function to ask the user how much they would like to purchase
+    function howManyToBuy() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "quantityToBuy",
+                message: "How many would you like to purchase?",
+                validate: function (input) {
+                    if (isNaN(input)) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        ]).then(function (user) {
+            quantityToBuy = parseInt(user.quantityToBuy);
+            // after user input is entered, call checkIfAvailable function
+            checkIfAvailable();
+        });
+    };
+    
+    // function to check if sufficient quantity of item
+    function checkIfAvailable() {
+        // query bamazon DB for the entered item and available quantity
+        con.query("SELECT stock_quantity, product_name, price FROM products WHERE id=?", [itemToBuy], function (err, res) {
+            if (err) throw err;
+            // if quantity entered is higher than quantity available, alert user there is insufficient stock then call startBamazon function to start over
+            else if (quantityToBuy > parseInt(res[0].stock_quantity)) {
+                console.log("Insufficient stock!");
+                console.log('\n*******************');
+                startBamazon();
+            } else {
+                // if quantity entered is equal or lower than quantity available, alert user of the purchase and total cost
+                console.log("Ok, you just purchased " + quantityToBuy + " " + res[0].product_name);
+                console.log("The total cost was: $" + (quantityToBuy * res[0].price));
+                console.log('\n*******************');
+                // call update Quantity function
+                updateQuantity();
+            }
+        });
+    }
+    
+    // function to update quantity in MySql
+    function updateQuantity() {
+        con.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+            [
+                quantityToBuy,
+                itemToBuy
+            ],
+            function (err, res) {
+                if (err) throw err;
+                updateProductSale();
+            })
+    };
+    
+    // function to update product sales
+    function updateProductSale() {
+        // select the item in database
+        con.query("SELECT price, id FROM products WHERE id =?", [itemToBuy], function (err, res) {
+            if (err) throw err;
+            // update sales of item in database
+            con.query("UPDATE products SET product_sales=product_sales + ? WHERE id=?",
+                [
+                    (parseFloat(res[0].price) * quantityToBuy),
+                    itemToBuy
+                ],
+                function (err, res) {
+                    if (err) throw err;
+                    startBamazon();
+                })
         }
-
-     
-        console.table(results);
-
-        
-        inquirer
-          .prompt([
-           {
-               type: 'input',
-               name: 'id',
-               message: 'What would you like to purchase?'
-           },
-           {
-            type: 'input',
-            name: 'quantity',
-            message: 'How many would you like to purchase?'
-        }
-          ])
-          .then(answers => {
-            console.log(answers, " --------------")
-            // Use user feedback for... whatever!!
-            processOrder(answers)
-          });
-        
-    })
-
-}
-
-
-function processOrder(answers) {
-    con.query('SELECT * FROM products WHERE id=' + answers.id, function(err, results){
-        console.log(results, "results in process order")
-        var stock_quantity = results[0].stock_quantity
-        if(answers.quantity > stock_quantity){
-            console.log("Sorry not enough in stock")
-        }else {
-            // update data base with new quantity
-
-        }
-
-    })
-}
-// inquirer
-//   .prompt([
-//    {
-//        type: 'input',
-//        name: 'test',
-//        message: 'say some thing'
-//    }
-//   ])
-//   .then(answers => {
-//       console.log(answers, "this is the answer")
-//     // Use user feedback for... whatever!!
-//   });
+        )
+    }
