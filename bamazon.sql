b44655b7d4b730f44be2283cbea4022c328d853a
mike929 <mike929@gmail.com>
2019-02-19 18:12:30 -0500

    updatedsqlfile

--- bamazon.sql
+++ bamazon.sql
@@ -2,16 +2,16 @@
 -- Query: SELECT * FROM bamazon.products
 LIMIT 0, 1000
 
--- Date: 2019-02-14 18:32
+-- Date: 2019-02-19 18:11
 */
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (1,'Canon 60D','Electronics',450,5);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (2,'Canon 50mm','Electronics',99.99,10);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (3,'Nikon 650D','Electronics',425.99,7);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (4,'Canon 70-200 2.8','Electronics',950.99,15);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (5,'Tamron SP 24-70mm','Electronics',799,8);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (6,'Canon EOS 6D Mark II','Electronics',1700,18);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (7,'Canon EOS 5D Mark IV','Electronics',2800,9);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (8,'Sony Alpha a7 Mirrorless Digital Camera','Electronics',1000.99,12);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (9,'Sony a7R III Mirrorless Camera','Electronics',3000,6);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (10,'Sony Planar T FE 50mm F1.4 ZA Lens','Electronics',1300,3);
-INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (11,'Sony FE 70-200mm f/2.8 GM OSS Lens','Electronics',2500,7);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (1,'Canon 60D','Electronics',450,13,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (2,'Canon 50mm','Electronics',99.99,9,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (3,'Nikon 650D','Electronics',425.99,7,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (4,'Canon 70-200 2.8','Electronics',950.99,15,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (5,'Tamron SP 24-70mm','Electronics',799,8,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (6,'Canon EOS 6D Mark II','Electronics',1700,18,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (7,'Canon EOS 5D Mark IV','Electronics',2800,9,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (8,'Sony Alpha a7 Mirrorless Digital Camera','Electronics',1000.99,12,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (9,'Sony a7R III Mirrorless Camera','Electronics',3000,6,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (10,'Sony Planar T FE 50mm F1.4 ZA Lens','Electronics',1300,15,NULL);
+INSERT INTO `` (`id`,`product_name`,`department_name`,`price`,`stock_quantity`,`product_sales`) VALUES (11,'Sony FE 70-200mm f/2.8 GM OSS Lens','Electronics',2500,17,NULL);
